23ba21ea9289a1ae1e482374e750745b908dd2c4
A2L5E0X1 <alexed25@protonmail.com>
2024-03-06 13:09:45 +0100

    mt6761-common: Bypass `native_packages.xml` copy checks

--- Android.mk
+++ Android.mk
@@ -8,4 +8,18 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter madrid, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
+$(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
+	@echo "Move vendor native_packages.bin to native_packages.xml"
+	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
+
+SYSTEM_NATIVE_PACKAGES_FIXUP := $(PRODUCT_OUT)/system/etc/native_packages.xml
+$(SYSTEM_NATIVE_PACKAGES_FIXUP): $(PRODUCT_OUT)/system/etc/native_packages.bin
+	@echo "Move system native_packages.bin to native_packages.xml"
+	$(hide) mv $(PRODUCT_OUT)/system/etc/native_packages.bin $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(NATIVE_PACKAGES_FIXUP) $(SYSTEM_NATIVE_PACKAGES_FIXUP)
+
 endif
