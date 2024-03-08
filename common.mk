b506567aad5ec6724c61ebe11186a227a54846c9
R0rt1z2 <me@r0rt1z2.com>
2024-03-08 21:29:52 +0100

    mt6761-common: Configure Gatekeeper stack

--- common.mk
+++ common.mk
@@ -31,6 +31,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service
