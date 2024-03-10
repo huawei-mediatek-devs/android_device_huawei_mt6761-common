1f4dcfb65eb6906a55c50508e8a1567b4044e6fc
R0rt1z2 <me@r0rt1z2.com>
2024-03-10 16:00:28 +0100

    mt6761-common: Configure Media (OMX) stack

--- common.mk
+++ common.mk
@@ -91,6 +91,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libxcollie
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp_policy/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
