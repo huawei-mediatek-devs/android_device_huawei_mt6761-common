9506ec97406ed507696497e611b5390ef7e609b2
Davide Garberi <dade.garberi@gmail.com>
2024-03-12 15:48:09 +0100

    mt6761-common: Build `libhidltransport` and `libhwbinder`
    Change-Id: Id87af3af701000734aaa89f1ec98a80dd70a266b

--- common.mk
+++ common.mk
@@ -40,6 +40,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
+    libhwbinder
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
